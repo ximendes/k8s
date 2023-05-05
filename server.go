57c097c9f1956e701be9d7edf4c20e0247506378
Eduardo  Ximendes <ximendes.eduardo@gmail.com>
2023-05-05 08:44:11 -0300

    first commit

---
+++ server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+var startedAt = time.Now()
+
+func main() {
+	http.HandleFunc("/", Hello)
+	http.ListenAndServe(":8000", nil)
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+	fmt.Fprintf(w, "Hello, I'm %s. I'm %s.", name, age)
+}
